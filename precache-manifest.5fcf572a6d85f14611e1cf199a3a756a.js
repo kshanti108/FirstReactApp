115257c12ef4bf5733e35a5b88361346cb804bf7
Tony Canelloni <simon@sambrady.co.uk>
2019-04-11 08:18:58 +0100

    Updates

---
+++ precache-manifest.5fcf572a6d85f14611e1cf199a3a756a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "3e1c4a0aa3b1466738ea",
+    "url": "/static/js/main.61156d41.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "3e1c4a0aa3b1466738ea",
+    "url": "/static/css/main.7b3d5403.chunk.css"
+  },
+  {
+    "revision": "8f27c09722665790b30d69d92062475a",
+    "url": "/index.html"
+  }
+];
